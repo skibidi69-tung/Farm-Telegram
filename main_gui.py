fb26b412e9cc7bd9b7db41979fba79d1ea3ce910
skibidi69-tung <93phamnguyenhoangpch@gmail.com>
2026-05-28 21:11:25 +0700

    Update main_gui.py

--- main_gui.py
+++ main_gui.py
@@ -24,9 +24,7 @@ TOOLS_RAW = {
     "ADS_TON_bot": "https://raw.githubusercontent.com/skibidi69-tung/Farm-Telegram/main/tools/ADS_TON_bot.py",
     "notbux_bot": "https://raw.githubusercontent.com/skibidi69-tung/Farm-Telegram/main/tools/notbux_bot.py",
     "EggsHatchBot": "https://raw.githubusercontent.com/skibidi69-tung/Farm-Telegram/main/tools/EggsHatchBot.py",
-    "treward_ton_bot": "https://raw.githubusercontent.com/skibidi69-tung/Farm-Telegram/main/tools/treward_ton_bot.py",
-    "GeneratorBot": "https://raw.githubusercontent.com/skibidi69-tung/Farm-Telegram/main/tools/GeneratorBot.py",
-    "FishVerseBot": "https://raw.githubusercontent.com/skibidi69-tung/Farm-Telegram/main/tools/FishVerseBot.py"
+    "GeneratorBot": "https://raw.githubusercontent.com/skibidi69-tung/Farm-Telegram/main/tools/GeneratorBot.py"
 }
 
 log_queue = queue.Queue()
